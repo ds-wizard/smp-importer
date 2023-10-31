4dad69e83bef55356195d4614dbb570824ce4f9c
Marek Suchánek <marek.suchanek@gmail.com>
2023-10-31 12:08:30 +0100

    Prepare file and URL inputs

--- src/smp_importer/app.py
+++ src/smp_importer/app.py
@@ -6,6 +6,10 @@ import fastapi
 import fastapi.responses
 import fastapi.staticfiles
 import fastapi.templating
+import httpx
+import pydantic
+
+from .logic import prepare_import_mapping
 
 
 ROOT_DIR = pathlib.Path(__file__).parent
@@ -23,6 +27,17 @@ app.mount(
 templates = fastapi.templating.Jinja2Templates(directory=TEMPLATES_DIR)
 
 
+class BodyURL(pydantic.BaseModel):
+    url: str
+
+
+class BodyFile(pydantic.BaseModel):
+    name: str
+    contents: str
+    type: str
+    bytesize: int
+
+
 @app.get('/', response_class=fastapi.responses.HTMLResponse)
 async def get_index(request: fastapi.Request):
     return templates.TemplateResponse(
@@ -35,10 +50,44 @@ async def get_index(request: fastapi.Request):
     )
 
 
-@app.get('/api/import', response_class=fastapi.responses.JSONResponse)
-async def list_fips(request: fastapi.Request):
+@app.post('/api/import-file', response_class=fastapi.responses.JSONResponse)
+async def api_import_from_file(body_file: BodyFile):
     try:
-        return fastapi.responses.JSONResponse(content={'text': 'Hello'})
+        result = prepare_import_mapping(
+            contents=body_file.contents,
+            content_type=body_file.type,
+        )
+        return fastapi.responses.JSONResponse(content={
+            'sourcce': 'file',
+            'name': body_file.name,
+            'actions': result['actions'],
+        })
     except Exception as e:
         LOG.error(f'Error appeared: {str(e)}', exc_info=e)
         raise fastapi.HTTPException(status_code=500)
+
+
+@app.post('/api/import-url', response_class=fastapi.responses.JSONResponse)
+async def api_import_from_url(body_url: BodyURL):
+    try:
+        result = await fetch_from_url(body_url.url)
+        return fastapi.responses.JSONResponse(content={
+            'source': 'url',
+            'url': body_url.url,
+            'actions': result['actions'],
+        })
+    except Exception as e:
+        LOG.error(f'Error appeared: {str(e)}', exc_info=e)
+        raise fastapi.HTTPException(status_code=500)
+
+
+# ----- logic
+
+async def fetch_from_url(url: str) -> dict:
+    async with httpx.AsyncClient() as client:
+        r = await client.get(url)
+        r.raise_for_status()
+        return prepare_import_mapping(
+            contents=r.content.decode(encoding=r.charset_encoding or 'utf-8'),
+            content_type=r.headers.get('content-type'),
+        )
---
+++ src/smp_importer/static/script.js
@@ -0,0 +1,131 @@
+const importer = new DSWImporter()
+
+importer
+    .init({
+        useWizardStyles: true,
+        windowSize: {
+            width: 300,
+            height: 500,
+        },
+    })
+    .then(() => {
+        jQuery('#file-input').on('input', function (e) {
+            const files = e.target.files
+            console.log(files)
+            const file = files[0]
+
+            const reader = new FileReader()
+                reader.addEventListener('load', (event) => {
+                    let data = ''
+                    try {
+                        data = event.target.result
+                    } catch (error) {
+                        alert('Failed to load file')
+                    }
+
+                    jQuery.ajax({
+                        type: 'POST',
+                        url: `/api/import-file`,
+                        data: JSON.stringify({
+                            'contents': data,
+                            'type': file.type,
+                            'name': file.name,
+                            'bytesize': file.size,
+                        }),
+                        contentType: "application/json; charset=utf-8",
+                        traditional: true,
+                        success: function (result) {
+                            doImport(result.actions)
+                        },
+                        error: function (result) {
+                            console.log(result)
+                            alert('failed')
+                        }
+                    })
+                })
+                reader.readAsText(file)
+        })
+
+        jQuery('#btn-load').on('click', function () {
+            const url = jQuery('#url-input').val()
+            console.log(url)
+
+            if (!isValidUrl(url)) {
+                alert('Invalid URL!')
+                jQuery('#url-input').val('')
+            } else {
+                jQuery.ajax({
+                    type: 'POST',
+                    url: `/api/import-url`,
+                    data: JSON.stringify({'url': url}),
+                    contentType: "application/json; charset=utf-8",
+                    traditional: true,
+                    success: function (result) {
+                        doImport(result.actions)
+                    },
+                    error: function (result) {
+                        console.log(result)
+                        alert('failed')
+                    }
+                })
+            }
+        })
+    })
+    .catch(error => {
+        console.error(error)
+    })
+
+
+function isValidUrl(urlString) {
+    const urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+        '(\\#[-a-z\\d_]*)?$','i') // validate fragment locator
+    return !!urlPattern.test(urlString)
+}
+
+function doImport(actions) {
+    const replacements = new Map()
+    const debug = jQuery('#debug-checkbox').is(':checked')
+    console.log(actions)
+
+    const replacePath = (path) => {
+        let newPath = [];
+        path.forEach((pathItem) => {
+            if (replacements.has(pathItem)) {
+                newPath.push(replacements.get(pathItem))
+            } else {
+                newPath.push(pathItem)
+            }
+        })
+        return newPath
+    }
+
+    actions.forEach((item) => {
+        if (item.type === 'debug') {
+            console.log('DEBUG:', item.message)
+        } else if (item.type === 'setReply') {
+            const path = replacePath(item.path)
+            console.log(`SET REPLY: ${path} = "${item.value}"`)
+            importer.setReply(path, item.value)
+        } else if (item.type === 'setIntegrationReply') {
+            const path = replacePath(item.path)
+            console.log(`SET INTEGRATION REPLY: ${path} = "${item.value}" [${item.itemId}]`)
+            importer.setIntegrationReply(path, item.value, item.itemId)
+        } else if (item.type === 'addItem') {
+            const path = replacePath(item.path)
+            const itemUuid = importer.addItem(path)
+            console.log(`ADD ITEM: ${path} = ${itemUuid} [${item.var}]`)
+            replacements.set(item.var, itemUuid)
+        }
+    })
+
+    if (debug) {
+        alert('Stop (debug enabled)')
+    }
+
+    importer.send()
+}
+
